3b200f83af1eee9e40fa987e9f366e70c1c7f42a
nullwijn <56175027+nullwijn@users.noreply.github.com>
2020-03-05 19:15:30 -0500

    support multiple airlines

--- src/capacity.cpp
+++ src/capacity.cpp
@@ -1,29 +1,71 @@
-//10% of the seats will be reserved for A-class
-//3% of the seats will be reserved for S-class
+//Each airline has a different percentage of high class seats.
 #include <iostream>
 #include <iomanip>
 #include <cmath>
 #include <string>
 
-int aClass(int capacity)
+int aClass(int capacity, int airChoice)
 {
-    return capacity * .1;
+    switch (airChoice)
+    {
+        case 1:
+            return capacity * .1;
+        case 2:
+            return capacity * .125;
+        case 3:
+            return capacity * .075;
+        case 4:
+            return capacity * .15;
+    }
 }
 
-int sClass(int capacity)
+int sClass(int capacity, int airChoice)
 {
-    return capacity * .03;
+    switch(airChoice)
+    {
+        case 1:
+            return capacity * .03;
+        case 2:
+            return capacity * .05;
+        case 3:
+            return capacity * .02;
+        case 4:
+            return capacity * .04;
+    }
 }
 
 int main()
 {
     int capacity;
+    int airChoice;
+    std::string airlines[] = {"Southwest Airlines", "Frontier Airlines", "American Airlines", "Delta Airlines"};
 
-    std::cout << "Enter the number of seats: ";
-    std::cin >> capacity;
-    std::cout << std::endl;
+    while(true)
+    {
+        std::cout << "Tristan's seat calculator\nPlease choose an airline." << 
+        "\n1\tSouthwest" <<
+        "\n2\tFrontier" << 
+        "\n3\tAmerican" <<
+        "\n4\tDelta" <<
+        "\n5\tExit Program" << std::endl;
+        std::cin >> airChoice;
 
-    std::cout << "# of A-Class seats: " << aClass(capacity) << " # of S-Class seats: " << sClass(capacity);
+        if(airChoice > 0 && airChoice < 5)
+        {
+            std::cout << "Enter the number of seats: ";
+            std::cin >> capacity;
+            std::cout << std::endl;
+
+            std::cout << airlines[airChoice-1] <<
+            "\nTotal # of seats: " << capacity <<
+            "\n# of A-Class seats: " << aClass(capacity, airChoice) <<
+            "\n# of S-Class seats: " << sClass(capacity, airChoice) << "\n\n";
+        }
+        else if(airChoice == 5)
+            break;
+        else
+            std::cout << "Invalid selection.";
+    }
 
     return 0;
 }
